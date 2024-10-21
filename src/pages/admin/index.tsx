1dd7f6a595db5863768644e6e449b5e59bd55772
eduardo <eduardo.moreirabernardi@gmail.com>
2024-10-21 17:02:10 -0300

    feat:....

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState, useEffect } from 'react'
 import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
+import { Input } from '../../components/Input';
 
 import { FiTrash } from 'react-icons/fi'
 import { db } from '../../services/firebaseConnection'
